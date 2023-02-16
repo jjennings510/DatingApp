4b381ac31d23eca3ab8c3c62bc557e8734bc0bef
Jacob <jacobjennings510@gmail.com>
2023-02-16 14:19:37 -0800

    Expanded API and added automapping

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,5 +1,6 @@
 using System.Reflection.Metadata.Ecma335;
 using API.Data;
+using API.Data.Repositories;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,8 @@ namespace API.Extensions
             });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
         }
---
+++ API/Extensions/DateTimeExcentions.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualBasic;
+
+namespace API.Extensions
+{
+    public static class DateTimeExcentions
+    {
+        public static int CalculateAge(this DateOnly dob)
+        {
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            var age = today.Year - dob.Year;
+
+            if (dob > today.AddYears(-age))
+                age--;
+
+            return age;
+        }
+    }
+}
